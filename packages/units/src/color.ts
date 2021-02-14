b136c0cc4602413731a334ba668c1eb8bfa7a791
noahluftyang <euihyun.yang.9x@gmail.com>
2021-02-14 17:31:53 +0900

    feat(units): add slack colors

--- packages/units/src/color.ts
+++ packages/units/src/color.ts
@@ -1,18 +1,24 @@
 export enum Color {
   // NOTE: red
   googleRed = '#ea4335',
+  slackRed = '#e01e5a',
   youtubeRed = '#ff0000',
+  // NOTE: yellow
+  slackYellow = '#ecb22e',
   googleYellow = '#fbbc05',
   kakaoYellow = '#ffcd00',
   // NOTE: green
-  naverGreen = '#00c63a',
-  spotifyGreen = '#1db954',
   googleGreen = '#34a853',
+  slackGreen = '#2eb67d',
+  spotifyGreen = '#1db954',
+  naverGreen = '#00c63a',
   // NOTE: blue
+  facebookBlue = '#3b5998',
+  slackBlue = '#36c5f0',
   tossBlue = '#317ff3',
   googleBlue = '#4285f4',
-  facebookBlue = '#3b5998',
   // NOTE: gray
   white = '#fff',
+  githubGray = '#f5f5f5',
   black = '#333',
 }
