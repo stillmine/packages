e7eba5714d812425611a15b8f364d57c203ce5d8
y09n <euihyun.yang.9x@gmail.com>
2021-05-23 17:30:26 +0900

    fix: modify package main / types (#143)
    * fix: modify package main / types
    
    * ci: add skip ci

--- packages/rollup-config/config/env.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require('dotenv');
-
-dotenv.config();
-
-module.exports = {
-  HOST: process.env.HOST || 'localhost',
-  NODE_ENV: process.env.NODE_ENV || 'development',
-  PORT: process.env.PORT || 3000,
-};
--- packages/rollup-config/config/path.js
+++
@@ -1,7 +0,0 @@
-const { resolve } = require('path');
-
-const rootDir = process.cwd();
-
-module.exports.packageJson = resolve(rootDir, 'package');
-module.exports.srcDir = resolve(rootDir, 'src', 'index');
-module.exports.rootDir = rootDir;
