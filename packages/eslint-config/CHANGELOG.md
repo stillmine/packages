4c50b2ab8cfd9b5d4e889c325fe56aa4f0147e7d
noahluftyang <euihyun.yang.9x@gmail.com>
2021-12-26 14:01:01 +0900

    feat(eslint-config): add next config

--- packages/eslint-config/CHANGELOG.md
+++ packages/eslint-config/CHANGELOG.md
@@ -1,5 +1,11 @@
 # Change Log
 
+## 1.3.0
+
+### Minor Changes
+
+- feat: add next config
+
 ## 1.2.0
 
 ### Minor Changes
--- packages/eslint-config/src/rules/react/index.js
+++ packages/eslint-config/src/rules/react/index.js
@@ -1,7 +1,14 @@
 const { resolveModule } = require('../../utils');
 
 module.exports = {
-  extends: ['./react/jsx', './react/jsx-a11y', './react/react', './react/react-hooks'].map(resolveModule),
+  extends: [
+    './react/emotion',
+    './react/jsx',
+    './react/jsx-a11y',
+    './react/next',
+    './react/react',
+    './react/react-hooks',
+  ].map(resolveModule),
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
