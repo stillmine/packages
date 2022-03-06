7b12c24660f7d58ee2a1c9c1607c4d499949e2bb
noahluftyang <euihyun.yang.9x@gmail.com>
2022-03-06 20:18:42 +0900

    feat(react-components): add external UI library

---
+++ .changeset/tricky-phones-protect.md
@@ -0,0 +1,5 @@
+---
+'@stillmine/react-components': minor
+---
+
+feat: add external UI library
--- packages/react-components/src/mod.ts
+++ packages/react-components/src/mod.ts
@@ -10,3 +10,43 @@ export * from './Stack/mod';
 export * from './Suspense/mod';
 export * from './Tab/mod';
 export * from './Text';
+export {
+  ThemeConfig,
+  Alert,
+  AlertIcon,
+  Badge,
+  Box,
+  Button,
+  ButtonGroup,
+  Code,
+  ColorModeScript,
+  Container,
+  extendTheme,
+  Flex,
+  Grid,
+  GridItem,
+  Heading,
+  HStack,
+  Input,
+  Link,
+  List,
+  ListItem,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  Select,
+  Skeleton,
+  Tab,
+  TabList,
+  Tabs,
+  UnorderedList,
+  useDisclosure,
+  usePrevious,
+  useToast,
+  VStack,
+} from '@chakra-ui/react';
+export { Dialog, Disclosure, Listbox, Menu, Popover, Switch } from '@headlessui/react';
+export { Checkbox } from '@nextui-org/react';
+export * from 'react-error-boundary';
